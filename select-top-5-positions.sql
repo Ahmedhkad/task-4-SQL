77b9820c0f598f4ce1dba8e499b60b21f11ee35a
ahmed <alomairi@mail.ru>
2022-11-12 13:12:41 +0300

    query ordered by position

--- select-top-5-positions.sql
+++ select-top-5-positions.sql
@@ -1,1 +1,2 @@
-SELECT `firstname`,`middle_name`,`lastname` FROM `sportsmen` WHERE `position` <= 5;
+SELECT `firstname`,`middle_name`,`lastname`, `position` FROM `sportsmen` WHERE `position` <= 5  
+ORDER BY `sportsmen`.`position` ASC
