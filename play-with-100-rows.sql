4e889907764518b7f72fb36fc1db7fb55f6d5115
ahmed <alomairi@mail.ru>
2022-11-12 13:37:05 +0300

    delete testing code (wrong commit)

--- play-with-100-rows.sql
+++ play-with-100-rows.sql
@@ -6,6 +6,3 @@ UPDATE sportsmen SET position = '8' WHERE id = 3;
 SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), birthday)), '%Y') + 0 AS age, * FROM sportsmen;     -- add age coloum claculted from birthday
 
 UPDATE sportsmen SET age = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), birthday)), '%Y') + 0 ;      -- update wrong age
-
-UPDATE sportsmen2 SET id = '88' WHERE position = 1;
-
